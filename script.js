65b5d97b122047cbe912c2393a3cfd5b328dbdc5
Alexander Dmitriev <test@example.com>
2023-02-21 22:08:37 +0300

    Login completed

---
+++ script.js
@@ -0,0 +1,5 @@
+let loginBtn = document.querySelector('.header-login-btn')
+let loginModal = document.querySelector('.login-modal')
+loginBtn.onclick = function() {
+    loginModal.style.display = 'block'
+}
